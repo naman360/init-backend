52496d3b25b932c862bf8b28802eced711dec0fb
naman360 <naman.geek@gmail.com>
2024-08-06 21:39:42 +0530

    feat: handled urls and query params

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
+
 const PORT = process.env.PORT || 8000;
+
 const server = http.createServer((req, res) => {
-  const logs = `${Date.now()}: Req received\n`;
+  const parsedUrl = url.parse(req.url, true);
+
+  const logs = `${Date.now()}: ${parsedUrl.path} Req received\n`;
   fs.appendFile("./src/assets/log.txt", logs, (err, data) => {
-    res.end("Hello world from Node server");
+    switch (parsedUrl.pathname) {
+      case "/":
+        res.end("Homepage");
+        break;
+      case "/contact-us":
+        res.end("Contact Us");
+        break;
+      case "/about":
+        const username = parsedUrl.query.username;
+        res.end(`Hi, ${username}`);
+        break;
+      default:
+        res.end("404 Not found");
+        break;
+    }
   });
 });
 
