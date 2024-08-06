277850c4694e94c61278ec9575df2d1ab01b072a
naman360 <naman.geek@gmail.com>
2024-08-06 21:21:30 +0530

    feat: working with fs module

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require("http");
+const fs = require("fs");
 const PORT = process.env.PORT || 8000;
 const server = http.createServer((req, res) => {
-  console.log("Request Received");
-  res.end("Hello world from Node server");
+  const logs = `${Date.now()}: Req received\n`;
+  fs.appendFile("./src/assets/log.txt", logs, (err, data) => {
+    res.end("Hello world from Node server");
+  });
 });
 
 server.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
